9279618a89e95cead0c2ad693837c5af5448e1dc
mdAshford <mashford@ua.edu>
2019-01-05 05:57:51 -0600

    Update BuildLib-CMake.sh

--- Projects/BuildLib-CMake.sh
+++ Projects/BuildLib-CMake.sh
@@ -46,6 +46,10 @@ if [ "$COOLP" == "1" ]; then
     popd
   else
     git clone --recursive https://github.com/BENGAL-TIGER/CoolPropMDA.git "$CP_SRC"
+    # fix-permissions CoolPropMDA
+    # git pull origin master
+    # git submodule init
+    # git submodule update
     # {mda) git clone -b goodExternals --single-branch --recursive https://github.com/BENGAL-TIGER/CoolProp.git "$CP_SRC"
     # (slamer59) git clone -b v6.1.0 --single-branch --recursive https://github.com/CoolProp/CoolProp.git "$CP_SRC"
     # git clone --recursive https://github.com/CoolProp/CoolProp.git "$CP_SRC"
